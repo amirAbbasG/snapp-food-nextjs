17f9e1f8ca8c09484be8964a235f6ab746aaca1e
amir <amir.h.a.g73@gmail.com>
2022-05-24 00:21:49 +0430

    add servuces base url to envirement variables

--- src/services/index.js
+++ src/services/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "http://localhost:3000/api/";
+axios.defaults.baseURL = process.env.NEXT_PUBLIC_API_URL
 
 const http = axios.create({
     withCredentials: true
